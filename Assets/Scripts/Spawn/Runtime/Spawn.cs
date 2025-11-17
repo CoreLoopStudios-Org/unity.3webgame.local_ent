c2413cf6902917943ea790b215c7ff6666d6095e
Tomal Tahsin Ahmmed <81379021+meButcher@users.noreply.github.com>
2025-11-17 11:50:23 +0600

    Changed to sunflower template
    Grid

---
+++ Assets/Scripts/Spawn/Runtime/Spawn.cs
@@ -0,0 +1,58 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+using VirtueSky.Events;
+
+public class Spawn : MonoBehaviour
+{
+    public Button prefab;
+    public EventNoParam eventNoParm;
+    public IntegerEvent clickedIndex;
+    
+
+    private void OnEnable()
+    {
+        eventNoParm.AddListener(Spawn100);
+    }
+    
+    private void OnDisable()
+    {
+        eventNoParm.RemoveListener(Spawn100);
+    }
+
+    [ContextMenu("Spawn")]
+    public void Spawn100()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            var button = Instantiate(prefab, transform.position, transform.rotation, transform);
+            int index = i; 
+            button.onClick.AddListener(() => OnButtonClick(index));
+            var gameObjectName = button.gameObject.name;
+            button.gameObject.name = String.Format(gameObjectName, i.ToString());
+        }
+    }
+
+    public void OnButtonClick(int i)
+    {
+        clickedIndex.Raise(i);
+    }
+
+
+    [ContextMenu("UnSpawn")]
+    public void UnSpawn()
+    {
+        // Remove all children
+        var transformChildCount = transform.childCount;
+        for (int i = transformChildCount - 1; i >= 0; i--)
+        {
+#if UNITY_EDITOR
+            DestroyImmediate(transform.GetChild(i).gameObject);
+#else
+            Destroy(transform.GetChild(i).gameObject);
+#endif
+        }
+        
+        
+    }
+}
